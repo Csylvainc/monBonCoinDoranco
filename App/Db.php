54ce8eff7e671b512ea5ba4fbde0f628884aa6da
Csylvainc <campos.sylvain@gmail.com>
2023-07-03 13:51:11 +0200

    with Db

--- App/Db.php
+++ App/Db.php
@@ -1,2 +1,23 @@
 <?php
 
+namespace App;
+
+use PDO;
+use PDOException;
+
+class Db{
+    private static $db;
+
+    static function getDb(){
+        if(!self::$db){
+            try{
+                $config = file_get_contents('../App/config.json');
+                $config = json_decode($config);
+                self::$db = new PDO("mysql:host=" . $config->host . ";dbname=". $config->dbname, $config->username, $config->password);
+            }catch(PDOException $e){
+                echo 'le fichier pose problème, erreur : '. $e->getMessage();
+            }
+        }
+        return self::$db;
+    }
+}
